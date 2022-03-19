b57316f6670016d6cf4fbbfeed5bbb4675049b03
L <EBogatyrevaPK@yandex.ru>
2022-03-19 18:41:55 +1200

    Я не уловила момент, где именно надо реализовать принцип полиморфизма))

---
+++ src/pro/sky/java/course2/Bicycle.java
@@ -0,0 +1,12 @@
+package pro.sky.java.course2;
+
+public class Bicycle extends TypeOfTransport {
+    public Bicycle(String modelName, int wheelsCount) {
+        super(modelName, wheelsCount);
+    }
+
+    public Bicycle() {
+        super();
+    }
+
+}
